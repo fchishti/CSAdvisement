818a4c232e6b20f4b5d9a82c1d1daa3e0126c1d5
fchishti-sw <fchishti-sw@CECE_SW_07.uco.local>
2017-03-21 10:25:15 -0500

    SQLScript added.

---
+++ TermProject_FaaezC_MakhdoomJ/src/java/SQLScript.sql
@@ -0,0 +1,45 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  fchishti-sw
+ * Created: Mar 21, 2017
+ */
+
+create table USERTABLE(
+    ID INT NOT NULL AUTO_INCREMENT,
+    FIRSTNAME varchar(255),
+    LASTNAME varchar(255),
+    PASSWORD char(64),
+    EMAIL varchar(255),
+    SCHOOL_ID INT,
+    PRIMARY KEY (id)
+);
+
+create table GROUPTABLE(
+    ID INT NOT NULL AUTO_INCREMENT,
+    USER_ID INT NOT NULL,
+    GROUPNAME varchar(255),
+    PRIMARY KEY(id)
+    FOREIGN KEY(USER_ID)
+        REFERENCES USERTABLE(ID)
+        ON DELETE CASCADE
+);
+
+insert into USERTABLE (FIRSTNAME, LASTNAME, PASSWORD, EMAIL, SCHOOL_ID)
+        values('Faaez','Chishti','c4289629b08bc4d61411aaa6d6d4a0c3c5f8c1e848e282976e29b6bed5aeedc7','fchishti@uco.edu',12345678);
+
+insert into GROUPTABLE (USER_ID,GROUPNAME) 
+        values ((SELECT USER_ID FROM USERINFO where FIRSTNAME = 'Faaez'),'studentgroup');
+
+insert into USERTABLE (FIRSTNAME, LASTNAME, PASSWORD, EMAIL)
+        values('Hong','Sung','c4289629b08bc4d61411aaa6d6d4a0c3c5f8c1e848e282976e29b6bed5aeedc7','hsung@uco.edu');
+
+insert into GROUPTABLE (USER_ID,GROUPNAME) 
+        values ((SELECT USER_ID FROM USERINFO where FIRSTNAME = 'Hong'),'facultygroup');
+
+insert into GROUPTABLE (USER_ID,GROUPNAME) 
+        values ((SELECT USER_ID FROM USERINFO where FIRSTNAME = 'Hong'),'admingroup');
+
