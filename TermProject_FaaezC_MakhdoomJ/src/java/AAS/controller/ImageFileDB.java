43cf3b804617964841b1bd5fedec69afe244d92e
fchishti-sw <fchishti-sw@CECE_SW_07.uco.local>
2017-04-12 13:45:43 -0500

    Added collider to walker and launcher

---
+++ TermProject_FaaezC_MakhdoomJ/src/java/AAS/controller/ImageFileDB.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AAS.controller;
+
+import AAS.model.ImageFile;
+import AAS.model.User;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.Part;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author fchishti-sw
+ */
+public class ImageFileDB {
+
+    private DataSource db;
+
+    public ImageFileDB(DataSource ds) {
+        this.db = ds;
+    }
+
+    public List<ImageFile> loadFileList() throws SQLException {
+
+        List<ImageFile> files = new ArrayList<>();
+        Connection conn = db.getConnection();
+
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet result = stmt.executeQuery(
+                    "SELECT ID, USER_ID, FILE_NAME, FILE_TYPE, FILE_SIZE FROM IMAGETABLE"
+            );
+            while (result.next()) {
+                ImageFile file = new ImageFile();
+                file.setId(result.getLong("ID"));
+                file.setName(result.getString("FILE_NAME"));
+                file.setType(result.getString("FILE_TYPE"));
+                file.setSize(result.getLong("FILE_SIZE"));
+                file.setUserId(result.getInt("USER_ID"));
+                files.add(file);
+            }
+        } finally {
+            conn.close();
+        }
+        return files;
+    }
+
+    public void uploadFile(Part part, User user) throws IOException, SQLException {
+
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+
+        Connection conn = db.getConnection();
+
+        InputStream inputStream;
+        inputStream = null;
+        try {
+            inputStream = part.getInputStream();
+            PreparedStatement insertQuery = conn.prepareStatement(
+                    "INSERT INTO IMAGETABLE (FILE_NAME, FILE_TYPE, FILE_SIZE, FILE_CONTENTS, USER_ID) "
+                    + "VALUES (?,?,?,?,?)");
+            insertQuery.setString(1, part.getSubmittedFileName());
+            insertQuery.setString(2, part.getContentType());
+            insertQuery.setLong(3, part.getSize());
+            insertQuery.setBinaryStream(4, inputStream);
+            insertQuery.setInt(5,user.getUserId());
+
+            int result = insertQuery.executeUpdate();
+            if (result == 1) {
+                facesContext.addMessage("uploadForm:upload",
+                        new FacesMessage(FacesMessage.SEVERITY_INFO,
+                                part.getSubmittedFileName()
+                                + ": uploaded successfuly !!", null));
+            } else {
+                // if not 1, it must be an error.
+                facesContext.addMessage("uploadForm:upload",
+                        new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                                result + " file uploaded", null));
+            }
+        } catch (IOException e) {
+            facesContext.addMessage("uploadForm:upload",
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                            "File upload failed !!", null));
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+    }
+    
+    public void delete(ImageFile imageFile) throws SQLException{
+        if (db == null) {
+            throw new SQLException("db is null; Can't get data source");
+        }
+
+        Connection conn = db.getConnection();
+
+        if (conn == null) {
+            throw new SQLException("conn is null; Can't get db connection");
+        }
+
+        try {
+            PreparedStatement ps = conn.prepareStatement(
+                    "delete from IMAGETABLE where ID = (?)"
+            );
+
+            ps.setInt(1, imageFile.getId());
+
+            int result = ps.executeUpdate();
+
+        } finally {
+            conn.close();
+        }
+    }
+}
---
+++ TermProject_FaaezC_MakhdoomJ/src/java/AAS/view/ImageFileBean.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AAS.view;
+
+import AAS.controller.ImageFileDB;
+import AAS.model.ImageFile;
+import AAS.utility.CurrentUser;
+import java.io.IOException;
+import java.io.Serializable;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+import javax.inject.Named;
+import javax.servlet.http.Part;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author fchishti-sw
+ */
+@Named
+@SessionScoped
+public class ImageFileBean implements Serializable {
+
+    private Part part;
+    private List<ImageFile> list;
+
+    @Resource(name = "jdbc/ds_wsp")
+    private DataSource ds;
+
+    private ImageFileDB imageFileDB;
+    private CurrentUser currentUser;
+
+    @PostConstruct
+    public void init() {
+        imageFileDB = new ImageFileDB(ds);
+        currentUser = new CurrentUser(ds);
+    }
+
+    public void updateList() throws SQLException {
+        try {
+            list = imageFileDB.loadFileList();
+        } catch (SQLException ex) {
+            Logger.getLogger(ImageFileBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void upload() throws SQLException {
+        try {
+            imageFileDB.uploadFile(part, currentUser.getUser());
+        } catch (SQLException | IOException ex) {
+            Logger.getLogger(ImageFileBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void validateFile(FacesContext ctx, UIComponent comp, Object value) {
+        if (value == null) {
+            throw new ValidatorException(
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                            "Select a file to upload", null));
+        }
+        Part file = (Part) value;
+        long size = file.getSize();
+        if (size <= 0) {
+            throw new ValidatorException(
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                            "the file is empty", null));
+        }
+        if (size > 1024 * 1024 * 10) { // 10 MB limit
+            throw new ValidatorException(
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                            size + "bytes: file too big (limit 10MB)", null));
+        }
+    }
+
+    public List<ImageFile> getList() throws SQLException {
+        return list;
+    }
+
+    public Part getPart() {
+        return part;
+    }
+
+    public void setPart(Part part) {
+        this.part = part;
+    }
+}
